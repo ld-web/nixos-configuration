f414f7ef1d9b37234bec91a79c39c188cc2c4f4e
ld-web <lucas@ld-web.net>
2025-11-24 15:05:10 +0100

    ZH, laravel, Beekeeper

--- configuration.nix
+++ configuration.nix
@@ -32,7 +32,7 @@
   time.timeZone = "Europe/Paris";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "zh_TW.UTF-8";
 
   i18n.supportedLocales = [
     "zh_TW.UTF-8/UTF-8"
@@ -42,15 +42,15 @@
   ];
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    LC_ADDRESS = "zh_TW.UTF-8";
+    LC_IDENTIFICATION = "zh_TW.UTF-8";
+    LC_MEASUREMENT = "zh_TW.UTF-8";
+    LC_MONETARY = "zh_TW.UTF-8";
+    LC_NAME = "zh_TW.UTF-8";
+    LC_NUMERIC = "zh_TW.UTF-8";
+    LC_PAPER = "zh_TW.UTF-8";
+    LC_TELEPHONE = "zh_TW.UTF-8";
+    LC_TIME = "zh_TW.UTF-8";
   };
 
   i18n.inputMethod = {
@@ -153,6 +153,11 @@
     };
   };
 
+  # Insecure packages
+  nixpkgs.config.permittedInsecurePackages = [
+    "beekeeper-studio-5.3.4"
+  ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs;
@@ -173,6 +178,7 @@
       '';
     };
   in [
+    unstable.beekeeper-studio
     unstable.bun
     chromium
     clang
@@ -203,6 +209,7 @@
     inxi # System information tool
     jq
     kdePackages.kdenlive
+    unstable.laravel
     libreoffice-fresh
     lshw # Hardware information
     microsoft-edge
