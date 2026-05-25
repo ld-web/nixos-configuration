3d4e38a61fe15beee22ffc3f8889f5f3bbd22347
ld-web <lucas@ld-web.net>
2026-05-25 06:04:57 +0200

    Flake

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,12 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, unstable, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      <home-manager/nixos>
     ];
 
   # Bootloader.
@@ -147,13 +146,15 @@
     localNetworkGameTransfers.openFirewall = true;
   };
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.packageOverrides = pkgs: {
-    unstable = import <nixos-unstable> {
-      config = config.nixpkgs.config;
-    };
-  };
+  #nixpkgs.config.packageOverrides = pkgs: {
+  #  unstable = import <nixos-unstable> {
+  #    config = config.nixpkgs.config;
+  #  };
+  #};
 
   # Insecure packages
   nixpkgs.config.permittedInsecurePackages = [
@@ -278,81 +279,6 @@
   # ZSH
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-
-  home-manager.users.lucas = { pkgs, ... }: {
-    # GNOME Extensions
-    dconf = {
-      enable = true;
-      settings."org/gnome/shell" = {
-        disable-user-extensions = false;
-        enabled-extensions = with pkgs.gnomeExtensions; [
-          system-monitor.extensionUuid
-          alttab-scroll-workaround.extensionUuid
-          appindicator.extensionUuid
-          just-perfection.extensionUuid
-          launch-new-instance.extensionUuid
-          lock-keys.extensionUuid
-          notification-timeout.extensionUuid
-          panel-world-clock-lite.extensionUuid
-          color-picker.extensionUuid
-        ];
-      };
-      settings."org/gnome/settings-daemon/plugins/media-keys" = {
-        custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-        ];
-      };
-      settings."org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        name = "Launch Console";
-        command = "kgx";
-        binding = "<Super>t";
-      };
-    };
-
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-    
-      shellAliases = {
-        update = "sudo nixos-rebuild switch";
-        ZH = "LANG='zh_TW.UTF-8'";
-        sail = "./vendor/bin/sail";
-      };
-      history = {
-        size = 10000;
-        path = "/home/lucas/zsh/history";
-      };    
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "bun" "composer" "copyfile" "copybuffer" "git" "history" "jsontools" "symfony6" "dirhistory" ];
-      };
-      plugins = [
-        {
-	  name = "powerlevel10k";
-	  src = pkgs.zsh-powerlevel10k;
-	  file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-	{
-          name = "powerlevel10k-config";
-    	  src = ./p10k-config;
-	  file = "p10k.zsh";
-	}
-      ];
-    };
-
-    programs.vim = {
-      enable = true;
-      extraConfig = ''
-        set nu
-        syn on
-        colo desert
-      '';
-    };
-
-    home.stateVersion = "24.05";
-  };
 
   # GIT
   programs.git.config = {
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  let
+    unstable = import nixpkgs-unstable {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+  in
+  {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        {
+          _module.args = {
+            inherit unstable;
+          };
+        }
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.lucas = ./home.nix;
+        }
+      ];
+    };
+  };
+}
